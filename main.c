12b88ea3d5d207f81fe253fbefe17e3fb568488d
brianpacouloute <bpacouloute@gmail.com>
2025-03-20 14:30:01 -0400

    Unique List function added

--- main.c
+++ main.c
@@ -1,4 +1,16 @@
 # include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+//  this will be used to see if a number is already in the list
+bool isUnique(int *uniqueList, int size, int num) {
+    for (int i = 0; i < size; i++) {
+        if (uniqueList[i] == num) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main() {
     printf("Hello, World!\n");
