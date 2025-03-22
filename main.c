c5a140587d58a78ad1cbe3c3625ce30eaa076f66
brianpacouloute <bpacouloute@gmail.com>
2025-03-22 13:54:51 -0400

    Complete Alg Implemented Ready for Code Review etc

--- main.c
+++ main.c
@@ -1,77 +1,164 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <stdbool.h>
-# include <mpi.h>
+/*
+Winter 2025
 
-# define MAX_ELEMENTS 100000
+Contributors:
+    Brian Pacouloute,
+    Ethan Chernitzky,
+    Thomas De Sa,
+    Ammar Ogeil
 
-//  this will be used to see if a number is already in the list
-bool is_unique(int *uniqueList, int size, int num) {
+Overview: 
+    Basically what we're doing here is giving each process a subset of rows if N = 10 and 4 processes process 0 might get 1-2 etc.
+    Each process computes the multiplication products in its rows. Now after this each process sends the product to the process that owns it.
+    To calculate who owns what product we use a modular operation:
+
+    owner = product % size;
+    ex:
+    product = 42; size = 4; 
+    owner = 42 % 4  = 2 
+    
+    so 42 is sent to process 2
+    each process receives a set of owned numbers and now the counting is done in the processes
+    */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <stdbool.h>
+
+// Constants
+# define DEFAULT_TABLE_SIZE 1000
+
+// Function to check if a number is already in the array
+bool isUnique(int *arr, int size, int num) {
     for (int i = 0; i < size; i++) {
-        if (uniqueList[i] == num) {
-            return false;
-        }
+        if (arr[i] == num) return false;
     }
     return true;
 }
 
-int merge_unique (int *dest, int *src, int dest_size, int src_size) {
-    for (int i = 0; i < src_size; i++) {
-        if (is_unique(dest, dest_size,src[i])) {
-            dest[dest_size++] = src[i];
-        }
+int main(int argc, char *argv[]) {
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // change to command line table_size input if none given use constant
+    int table_size = 0;
+    if (argc != 2) {
+        if (rank == 0) printf("Usage: %s <table_size> : Using default table size 1000\n", argv[0]);
+        table_size = DEFAULT_TABLE_SIZE;
+    } else {
+        table_size = atoi(argv[1]);
     }
-    return dest_size;
-}
 
-// function to count unique elements in the multiplication table
-int count_unique_elements(int N) {
-    int *unique_list = (int *) malloc(N * N * sizeof(int)); // store unique elements
-    int unique_count = 0; // count of unique elements
+    MPI_Bcast(&table_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // generate multiplication table
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
+    int rowsPerProc = table_size / size;
+    int start = rank * rowsPerProc + 1;
+    int end = (rank == size - 1) ? table_size : start + rowsPerProc - 1;
+
+    // Allocate and prepare buffers for sending to each process
+    int **sendBuffers = malloc(size * sizeof(int *));
+    int *sendCounts = calloc(size, sizeof(int));
+    int *sendCapacities = malloc(size * sizeof(int));
+
+    for (int i = 0; i < size; i++) {
+        sendCapacities[i] = 100;
+        sendBuffers[i] = malloc(sendCapacities[i] * sizeof(int));
+    }
+
+    for (int i = start; i <= end; i++) {
+        for (int j = 1; j <= table_size; j++) {
             int product = i * j;
-            if (is_unique(unique_list, unique_count, product)) {
-                unique_list[unique_count++] = product;
+            int owner = product % size;
+            if (sendCounts[owner] == sendCapacities[owner]) {
+                sendCapacities[owner] *= 2;
+                sendBuffers[owner] = realloc(sendBuffers[owner], sendCapacities[owner] * sizeof(int));
+                if (!sendBuffers[owner]) {
+                    fprintf(stderr, "Realloc failed on process %d\n", rank);
+                    MPI_Abort(MPI_COMM_WORLD, 1);
+                }
             }
+            sendBuffers[owner][sendCounts[owner]++] = product;
         }
     }
 
-    free(unique_list);
-    return unique_count;
-}
+    // Exchange send counts
+    int *recvCounts = malloc(size * sizeof(int));
+    MPI_Alltoall(sendCounts, 1, MPI_INT, recvCounts, 1, MPI_INT, MPI_COMM_WORLD);
 
-int main(int arg_count, char *arg_values[]) {
-    int rank, size, N;
+    // Calculate displacements
+    int *sendDispls = malloc(size * sizeof(int));
+    int *recvDispls = malloc(size * sizeof(int));
+    int totalSend = 0, totalRecv = 0;
 
-    MPI_Init(&arg_count, &arg_values);
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    MPI_Comm_size(MPI_COMM_WORLD,&size);
-
-    // Temporary N until i switch to command line argument N
-    if (rank == 0) {
-        printf("Enter the value of N: ");
-        fflush(stdout);
-        scanf("%d", &N);
+    for (int i = 0; i < size; i++) {
+        sendDispls[i] = totalSend;
+        recvDispls[i] = totalRecv;
+        totalSend += sendCounts[i];
+        totalRecv += recvCounts[i];
     }
 
-    // broadcast N to all processors
-    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    // Flatten send buffers
+    int *sendBuffer = malloc(totalSend * sizeof(int));
+    int pos = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < sendCounts[i]; j++) {
+            sendBuffer[pos++] = sendBuffers[i][j];
+        }
+        free(sendBuffers[i]);
+    }
+    free(sendBuffers);
+    free(sendCapacities);
 
-    // computer row range for this process
-    int rows_per_proc = N / size;
-    int start = rank * rows_per_proc + 1;
-    int end = (rank == size - 1) ? N : start + rows_per_proc - 1;
+    int *recvBuffer = malloc(totalRecv * sizeof(int));
 
-    // what we will go for is a local computationn of products then has each product to a process based on a value range
-    // then send products to their owner process
-    // each process can then dup it's own received values each process counts it unique products
-    // MPI_Reduce
-    // this would be faster than seperating everything into it's many processes and then checking for dupes then joining everything and checking for dupes again (uneffective)
+    // All-to-all communication
+    MPI_Alltoallv(sendBuffer, sendCounts, sendDispls, MPI_INT,
+                  recvBuffer, recvCounts, recvDispls, MPI_INT,
+                  MPI_COMM_WORLD);
 
+    // Now it's safe to free sendCounts
+    free(sendCounts);
+    free(sendBuffer);
+    free(sendDispls);
+    free(recvDispls);
 
-    printf("The number of unique elements in the multiplication table of size %d is %d\n", N, count_unique_elements(N));
+    // Deduplicate
+    int *localUnique = malloc(100 * sizeof(int));
+    int localCount = 0, localCapacity = 100;
+
+    for (int i = 0; i < totalRecv; i++) {
+        int product = recvBuffer[i];
+        if (isUnique(localUnique, localCount, product)) {
+            if (localCount == localCapacity) {
+                localCapacity *= 2;
+                localUnique = realloc(localUnique, localCapacity * sizeof(int));
+                if (!localUnique) {
+                    fprintf(stderr, "Realloc failed in dedup on process %d\n", rank);
+                    MPI_Abort(MPI_COMM_WORLD, 1);
+                }
+            }
+            localUnique[localCount++] = product;
+        }
+    }
+
+    free(recvBuffer);
+
+    // Reduce total unique count
+    int globalCount;
+    MPI_Reduce(&localCount, &globalCount, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        printf("Total unique elements in %dx%d multiplication table: %d\n", table_size, table_size, globalCount);
+    }
+
+    free(localUnique);
+    free(recvCounts);
+
+    MPI_Finalize();
     return 0;
 }
