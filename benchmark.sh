2f21bf6e62937dc6411da677325b53eff8553c56
tomcat <desa2thomas@gmail.com>
2025-03-27 11:07:11 -0400

    small change to benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -11,8 +11,8 @@
 module purge
 module load StdEnv/2023 gcc/12.3 openmpi/4.1.5
 
-array=(80 160 640 1000 2000 8000 16000 32000 40000 50000 60000 70000 80000 90000 100000)
-for procs in {30..50..5}
+array=(20000)
+for procs in {10..50..5}
 do
   for N in "${array[@]}"
   do
